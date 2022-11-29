efd4eeab57c4b422db5a6adef44f3431fc0ac373
chenxixin <chenxixin>
2022-11-29 15:45:42 +0800

    update

---
+++ js/powerInfo/hoisting-machinery-power.js
@@ -0,0 +1,14 @@
+
+
+export {
+  towerCraneCompanyPower, towerCraneProjectPower,
+  elevatorCompanyPower, elevatorProjectPower
+}
+
+项目级
+实时监控
+/home/alot_project/tower-crane/realTimeMonitoring_project
+
+公司级
+实时监控
+/home/alot_company/elevator/alot_company_elevator_monitor
